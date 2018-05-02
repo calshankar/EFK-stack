b1cbd48fcf236593fcc8dae07a7c8e7ea925ddf4
Shankar <callshank_78@yahoo.com>
2018-05-02 10:30:06 +0530

    Changes to run the apm & app on localhost

--- code/index.js
+++ code/index.js
@@ -4,7 +4,7 @@ var apm = require('elastic-apm-node').start({
     // Use if APM Server requires a token
     secretToken: '',
     // Set custom APM Server URL (default: http://localhost:8200)
-    serverUrl: 'http://127.0.0.1:8200'
+    serverUrl: 'http://localhost:8200'
 })
 
 const express = require('express'),
